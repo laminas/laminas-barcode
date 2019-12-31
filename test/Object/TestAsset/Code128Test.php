8ef5c1731841db4c7317404847fa061e0bc98a7a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:17:50 -0600

    Rewriting as Laminas Project package

--- test/Object/TestAsset/Code128Test.php
+++ test/Object/TestAsset/Code128Test.php
@@ -1,18 +1,17 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
-namespace ZendTest\Barcode\Object\TestAsset;
 
 /**
- * @group      Zend_Barcode
+ * @see       https://github.com/laminas/laminas-barcode for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-barcode/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-barcode/blob/master/LICENSE.md New BSD License
  */
-class Code128Test extends \Zend\Barcode\Object\Code128
+
+namespace LaminasTest\Barcode\Object\TestAsset;
+
+/**
+ * @group      Laminas_Barcode
+ */
+class Code128Test extends \Laminas\Barcode\Object\Code128
 {
     public function convertToBarcodeChars($string)
     {
