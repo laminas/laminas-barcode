d4d66cbf45aac1ad2423ecec810be8cc41b9ac1a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:16:40 -0600

    Rewriting as Laminas Project package

--- src/Barcode.php
+++ src/Barcode.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-barcode for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-barcode/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-barcode/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Barcode;
+namespace Laminas\Barcode;
 
+use Laminas\Stdlib\ArrayUtils;
 use Traversable;
-use Zend\Stdlib\ArrayUtils;
 
 /**
  * Class for generate Barcode
@@ -71,7 +70,7 @@ abstract class Barcode
     }
 
     /**
-     * Factory for Zend\Barcode classes.
+     * Factory for Laminas\Barcode classes.
      *
      * First argument may be a string containing the base of the adapter class
      * name, e.g. 'code25' corresponds to class Object\Code25.  This
