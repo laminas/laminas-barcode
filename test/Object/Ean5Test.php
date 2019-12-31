8a068b2a4132cffe8b793cab5a46904b2fe814ef
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:15:49 -0600

    Rewriting as Laminas Project package

--- test/Object/Ean5Test.php
+++ test/Object/Ean5Test.php
@@ -1,22 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Barcode
+ * @see       https://github.com/laminas/laminas-barcode for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-barcode/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-barcode/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Barcode\Object;
+namespace LaminasTest\Barcode\Object;
 
-use Zend\Barcode;
+use Laminas\Barcode;
 
 /**
- * @category   Zend
- * @package    Zend_Barcode
+ * @category   Laminas
+ * @package    Laminas_Barcode
  * @subpackage UnitTests
- * @group      Zend_Barcode
+ * @group      Laminas_Barcode
  */
 class Ean5Test extends TestCommon
 {
