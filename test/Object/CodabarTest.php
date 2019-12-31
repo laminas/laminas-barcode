3da9ee3f9890d59c62503c2999f35768f9313da6
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:17:03 -0600

    Rewriting as Laminas Project package

--- test/Object/CodabarTest.php
+++ test/Object/CodabarTest.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-barcode for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-barcode/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-barcode/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Barcode\Object;
+namespace LaminasTest\Barcode\Object;
 
-use Zend\Barcode;
+use Laminas\Barcode;
 
 /**
- * @group      Zend_Barcode
+ * @group      Laminas_Barcode
  */
 class CodabarTest extends TestCommon
 {
