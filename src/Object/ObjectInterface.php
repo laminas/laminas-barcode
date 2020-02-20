b83d55d2a2d4742e0fa5ba64c64807e985657656
simivar <simivar@gmail.com>
2020-02-20 22:57:20 +0100

    Re-introduce ObjectInterface in old namespace that extends new BarcodeInterface

---
+++ src/Object/ObjectInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-barcode for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-barcode/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-barcode/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Laminas\Barcode\Object;
+
+use Laminas\Barcode\Barcode\BarcodeInterface;
+
+/**
+ * Interface for generate Barcode
+ */
+interface ObjectInterface extends BarcodeInterface
+{
+}
